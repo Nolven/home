fd1943876505568cbf0e9bf09bc5a9d4a951451c
Nolv <nolvenon@gmail.com>
2021-04-13 10:31:25 +0300

    Gradient update

--- androidApp/app/src/main/java/com/example/homecontrole/Gradient.kt
+++ androidApp/app/src/main/java/com/example/homecontrole/Gradient.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.LinearLayout
 import androidx.core.graphics.blue
 import androidx.core.graphics.green
 import androidx.core.graphics.red
@@ -23,23 +22,21 @@ import java.lang.Integer.parseInt
 
 class Gradient(private val view: View, private val context: Context)
 {
-    private var layoutIDs: ArrayList<Int> = ArrayList()
-    private var clearIDs: ArrayList<Int> = ArrayList()
+    private var colorPickerIds: ArrayList<Int> = ArrayList()
 
     private val data: GradientData = GradientData(0, 0, 0, arrayListOf())
 
     private fun removeColor()
     {
-        view.led_color_gradient_host.removeView(view.findViewById(layoutIDs.last()))
+        view.led_color_gradient_host.removeView(view.findViewById(colorPickerIds.last()))
 
         // Remove last id's
         data.colors.removeAt(data.colors.size - 1)
-        layoutIDs.removeAt(layoutIDs.size - 1)
-        clearIDs.removeAt(clearIDs.size - 1)
+        colorPickerIds.removeAt(colorPickerIds.size - 1)
 
-        // Show clear button
-        if( clearIDs.isNotEmpty() )
-            view.findViewById<Button>(clearIDs.last()).visibility = View.VISIBLE
+        // Hide clear button when there is no colors
+        if( colorPickerIds.isEmpty() )
+            view.gradient_remove_color.visibility = View.INVISIBLE
     }
 
     private fun addColor()
@@ -47,15 +44,11 @@ class Gradient(private val view: View, private val context: Context)
         val colorIndex = data.colors.size
         data.colors.add(arrayOf(255, 0, 255))
 
-        // Create new layout for 2 buttons
-        val ll = LinearLayout(context)
-        val layoutId = View.generateViewId()
-        layoutIDs.add(layoutId)
-        ll.id = layoutId
-        ll.orientation = LinearLayout.HORIZONTAL
-
         // Create color button
         val colorButton = Button(context)
+        val buttonId = View.generateViewId()
+        colorButton.id = buttonId
+        colorPickerIds.add(buttonId)
         colorButton.setBackgroundColor(0xFFFF00FF.toInt())
         colorButton.setOnClickListener{
             val initialColor = Color.rgb(
@@ -85,23 +78,12 @@ class Gradient(private val view: View, private val context: Context)
                 .build()
                 .show()
         }
-        ll.addView(colorButton)
 
-        // Hide previous clear button
-        if (clearIDs.isNotEmpty())
-            view.findViewById<Button>(clearIDs.last()).visibility = View.INVISIBLE
+        view.led_color_gradient_host.addView(colorButton)
 
-        // Create clear button
-        // ID is for hiding button add new color addition
-        val clearButton = Button(context)
-        val clearId = View.generateViewId()
-        clearIDs.add(clearId)
-        clearButton.id = clearId
-        clearButton.text = context.resources.getText(R.string.led_gradient_clear)
-        clearButton.setOnClickListener{ removeColor() }
-        ll.addView(clearButton)
-
-        view.led_color_gradient_host.addView(ll)
+        // Show clear button
+        if (view.gradient_remove_color.visibility == View.INVISIBLE)
+            view.gradient_remove_color.visibility = View.VISIBLE
     }
 
     private fun setDataUpdateCb()
@@ -111,7 +93,8 @@ class Gradient(private val view: View, private val context: Context)
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 
             override fun afterTextChanged(s: Editable?) {
-                data.speed = parseInt(view.gradient_speed.text.toString())
+                if( s.toString().isNotEmpty() )
+                    data.speed = parseInt(view.gradient_speed.text.toString())
             }
         })
 
@@ -120,7 +103,8 @@ class Gradient(private val view: View, private val context: Context)
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 
             override fun afterTextChanged(s: Editable?) {
-                data.sampler_step = parseInt(view.gradient_sampler_step.text.toString())
+                if( s.toString().isNotEmpty() )
+                    data.sampler_step = parseInt(view.gradient_sampler_step.text.toString())
             }
         })
 
@@ -148,7 +132,8 @@ class Gradient(private val view: View, private val context: Context)
     }
 
     init {
-        view.gradient_add_color_button.setOnClickListener { addColor() }
+        view.gradient_add_color.setOnClickListener { addColor() }
+        view.gradient_remove_color.setOnClickListener{ removeColor() }
         setDataUpdateCb()
 
        ArrayAdapter.createFromResource(
